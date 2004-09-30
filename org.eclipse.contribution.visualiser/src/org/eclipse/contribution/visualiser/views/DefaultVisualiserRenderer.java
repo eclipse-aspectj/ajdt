af68fb063775fc771cb127cef8059dbc301b236f
mchapman <mchapman>
2004-09-30 11:31:34 +0000

    New default rendering style for the visualiser. The old style is available as "classic".

--- org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/views/DefaultVisualiserRenderer.java
+++ org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/views/DefaultVisualiserRenderer.java
@@ -10,23 +10,24 @@
  *******************************************************************************/
 package org.eclipse.contribution.visualiser.views;
 
-import org.eclipse.contribution.visualiser.VisualiserPlugin;
 import org.eclipse.contribution.visualiser.interfaces.IMember;
-import org.eclipse.contribution.visualiser.interfaces.IVisualiserRenderer;
 import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
 
 /**
- * Implements the visualiser renderer interface to provide the classic
- * visualiser drawing style. This is the default renderer. Custom renderers can
- * either implement the interface directly or extend this class.
+ * Implements the visualiser renderer interface to provide the default
+ * visualiser drawing style, which has plain columns plus Eclipse style headers
+ * with rounded corners. Custom renderers can either implement the interface
+ * directly or extend this class, or any of the other renderers.
  * 
  * @author mchapman
  */
-public class DefaultVisualiserRenderer implements IVisualiserRenderer {
+public class DefaultVisualiserRenderer extends ClassicVisualiserRenderer {
 
 	private int spacing = 4;
 
@@ -35,16 +36,30 @@ public class DefaultVisualiserRenderer implements IVisualiserRenderer {
 	private int margin = 6;
 
 	private Font sysFont;
-	
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.contribution.visualiser.interfaces.IVisualiserRenderer#getSpacing()
-	 */
-	public int getSpacing() {
-		return spacing;
-	}
+
+	protected Color outlineColor = new Color(Display.getDefault(), 172, 168,
+			153);
+
+	protected Color outlineColor2 = new Color(Display.getDefault(), 214, 211,
+			195);
+
+	protected Color lightGrayGradientColor = new Color(Display.getDefault(),
+			230, 230, 230);
+
+	protected Color[] colorList = new Color[] { VisualiserCanvas.VIS_BG_COLOUR,
+			outlineColor2, outlineColor };
+
+	// data for constructing a left-hand rounded corner with title gradient
+	protected static int cornerSize = 6;
+
+	protected static byte[][] cornerData = new byte[][] { { 1, 5, 0, 5, 0 },
+			{ 0, 1, 1, 2, 1 }, { 1, 3, 1, 3, 1 }, { 2, 4, 1, 5, 1 },
+			{ 0, 1, 2, 1, 2 }, { 1, 2, 2, 2, 2 }, { 2, 3, 2, 3, 2 },
+			{ 1, 1, 3, 1, 3 }, { 2, 2, 3, 2, 3 }, { 1, 0, 4, 0, 5 },
+			{ 2, 1, 4, 1, 5 } };
+
+	// store the rounded corners image data, to save recreating it each time
+	private static ImageData header;
 
 	/*
 	 * (non-Javadoc)
@@ -52,27 +67,40 @@ public class DefaultVisualiserRenderer implements IVisualiserRenderer {
 	 * @see org.eclipse.contribution.visualiser.views.VisualiserRendering#getColumnHeaderHeight()
 	 */
 	public int getColumnHeaderHeight() {
-		if (columnTitleHeight < 0) {
-			sysFont = Display.getCurrent().getSystemFont();
-			Image tmpImg = new Image(Display.getCurrent(),1,1);
-			GC gc = new GC(tmpImg);
-			gc.setFont(sysFont);
-			String s = VisualiserPlugin.getResourceString("Visualiser.testString"); //$NON-NLS-1$
-			// determine string height, and scale up a little to increase padding
-			columnTitleHeight = (gc.stringExtent(s).y * 11)/10 + 4;
-			gc.dispose();
-			tmpImg.dispose();
-		}
-		return columnTitleHeight;
+		return super.getColumnHeaderHeight() + 2;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.contribution.visualiser.interfaces.IVisualiserRenderer#getMarginSize()
-	 */
-	public int getMarginSize() {
-		return margin;
+	private void createHeader(int width) {
+		Image headerImg = new Image(Display.getDefault(), width,
+				getColumnHeaderHeight());
+		GC gc = new GC(headerImg);
+
+		gc.setBackground(colorList[0]);
+		gc.fillRectangle(0, 0, width, getColumnHeaderHeight());
+
+		// draw gradient
+		gc.setForeground(ColorConstants.titleBackground);
+		gc.setBackground(ColorConstants.titleGradient);
+		gc.fillGradientRectangle(1, 1, width - 1, getColumnHeaderHeight() - 1,
+				true);
+
+		// draw left-hand corner
+		for (int i = 0; i < cornerData.length; i++) {
+			byte[] b = cornerData[i];
+			gc.setForeground(colorList[b[0]]);
+			gc.drawLine(b[1], b[2], b[3], b[4]);
+		}
+
+		// draw right-hand corner
+		for (int i = 0; i < cornerData.length; i++) {
+			byte[] b = cornerData[i];
+			gc.setForeground(colorList[b[0]]);
+			gc.drawLine(width - b[1], b[2], width - b[3], b[4]);
+		}
+
+		header = headerImg.getImageData();
+		gc.dispose();
+		headerImg.dispose();
 	}
 
 	/*
@@ -85,23 +113,44 @@ public class DefaultVisualiserRenderer implements IVisualiserRenderer {
 		int title_y_start = getMarginSize();
 		int title_y_height = getColumnHeaderHeight();
 
-		gc.setBackground(ColorConstants.buttonDarker);
-		gc.fillRectangle(x, title_y_start, colWidth, title_y_height);
+		if ((header == null) || (header.width != colWidth)) {
+			createHeader(colWidth);
+		}
+		Image headerImg = new Image(Display.getDefault(), header);
+		gc.drawImage(headerImg, x, title_y_start);
+		headerImg.dispose();
+
 		String name = getConstrainedString(gc, m.getName(), colWidth - 4);
 		int xoff = (colWidth - gc.stringExtent(name).x) / 2;
-		if (sysFont==null) {
-			sysFont = Display.getCurrent().getSystemFont();
-		}
-		gc.setFont(sysFont);
-		gc.drawString(name, x + xoff, title_y_start + 4, true);
-
-		// draw 3d effect
 		gc.setForeground(ColorConstants.white);
-		gc.drawLine(x, title_y_start, x, title_y_start + title_y_height);
-		gc.drawLine(x, title_y_start, x + colWidth, title_y_start);
+		gc.drawString(name, x + xoff, title_y_start + 4, true);
 		gc.setForeground(ColorConstants.black);
-		gc.drawLine(x + colWidth, title_y_start, x + colWidth, title_y_start
+
+		gc.setForeground(outlineColor);
+		gc.drawLine(x, title_y_start + cornerSize, x, title_y_start
 				+ title_y_height);
+		gc.drawLine(x + cornerSize, title_y_start, x + colWidth - cornerSize,
+				title_y_start);
+		gc.drawLine(x + colWidth, title_y_start + cornerSize, x + colWidth,
+				title_y_start + title_y_height);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.contribution.visualiser.interfaces.IVisualiserRenderer#getSpacing()
+	 */
+	public int getSpacing() {
+		return spacing;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.contribution.visualiser.interfaces.IVisualiserRenderer#getMarginSize()
+	 */
+	public int getMarginSize() {
+		return margin;
 	}
 
 	/*
@@ -120,36 +169,6 @@ public class DefaultVisualiserRenderer implements IVisualiserRenderer {
 		}
 		gc.fillRectangle(x, yoff, colWidth, colHeight);
 		gc.drawRectangle(x, yoff, colWidth, colHeight);
-	}
-
-	/*
-	 * Shortens the given string if necessary to fit the given width
-	 */
-	protected String getConstrainedString(GC gc, String name, int width) {
-		String elip = "..."; //$NON-NLS-1$
-		if (width <= gc.stringExtent(elip).x) {
-			return elip;
-		}
-		int sw = gc.stringExtent(name).x;
-		boolean needElipsis = false;
-		int targetWidth = width;
-		while (sw > targetWidth) {
-			if (!needElipsis) {
-				needElipsis = true;
-				targetWidth -= gc.stringExtent(elip).x;
-			}
-			name = name.substring(0, name.length() - 1);
-			if (name.length() <= 1) {
-				// couldn't make the string short enough, will just have to clip
-				// the dots
-				return elip;
-			}
-			sw = gc.stringExtent(name).x;
-		}
-		if (needElipsis) {
-			name += elip;
-		}
-		return name;
 	}
 
 }
--- org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/views/GradientVisualiserRenderer.java
+++ org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/views/GradientVisualiserRenderer.java
@@ -12,51 +12,14 @@ package org.eclipse.contribution.visualiser.views;
 
 import org.eclipse.contribution.visualiser.interfaces.IMember;
 import org.eclipse.draw2d.ColorConstants;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.widgets.Display;
 
 /**
- * Like the "classic" style, but gradient filled, with rounded corner titles.
+ * Just like the "default" style, but gradient filled.
  * 
  * @author mchapman
  */
 public class GradientVisualiserRenderer extends DefaultVisualiserRenderer {
-
-	protected Color outlineColor = new Color(Display.getDefault(), 172, 168,
-			153);
-
-	protected Color outlineColor2 = new Color(Display.getDefault(), 214, 211,
-			195);
-
-	protected Color lightGrayGradientColor = new Color(Display.getDefault(),
-			230, 230, 230);
-
-	protected Color[] colorList = new Color[] { VisualiserCanvas.VIS_BG_COLOUR,
-			outlineColor2, outlineColor };
-
-	// data for constructing a left-hand rounded corner with title gradient
-	protected static int cornerSize = 6;
-
-	protected static byte[][] cornerData = new byte[][] { { 1, 5, 0, 5, 0 },
-			{ 0, 1, 1, 2, 1 }, { 1, 3, 1, 3, 1 }, { 2, 4, 1, 5, 1 },
-			{ 0, 1, 2, 1, 2 }, { 1, 2, 2, 2, 2 }, { 2, 3, 2, 3, 2 },
-			{ 1, 1, 3, 1, 3 }, { 2, 2, 3, 2, 3 }, { 1, 0, 4, 0, 5 },
-			{ 2, 1, 4, 1, 5 } };
-
-	// store the rounded corners image data, to save recreating it each time
-	private static ImageData header;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.contribution.visualiser.views.VisualiserRendering#getColumnHeaderHeight()
-	 */
-	public int getColumnHeaderHeight() {
-		return super.getColumnHeaderHeight() + 2;
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -80,69 +43,5 @@ public class GradientVisualiserRenderer extends DefaultVisualiserRenderer {
 		gc.drawRectangle(x, yoff, colWidth, colHeight);
 	}
 
-	private void createHeader(int width) {
-		Image headerImg = new Image(Display.getDefault(), width,
-				getColumnHeaderHeight());
-		GC gc = new GC(headerImg);
-
-		gc.setBackground(colorList[0]);
-		gc.fillRectangle(0, 0, width, getColumnHeaderHeight());
-
-		// draw gradient
-		gc.setForeground(ColorConstants.titleBackground);
-		gc.setBackground(ColorConstants.titleGradient);
-		gc.fillGradientRectangle(1, 1, width - 1, getColumnHeaderHeight() - 1,
-				true);
-
-		// draw left-hand corner
-		for (int i = 0; i < cornerData.length; i++) {
-			byte[] b = cornerData[i];
-			gc.setForeground(colorList[b[0]]);
-			gc.drawLine(b[1], b[2], b[3], b[4]);
-		}
-
-		// draw right-hand corner
-		for (int i = 0; i < cornerData.length; i++) {
-			byte[] b = cornerData[i];
-			gc.setForeground(colorList[b[0]]);
-			gc.drawLine(width - b[1], b[2], width - b[3], b[4]);
-		}
-
-		header = headerImg.getImageData();
-		gc.dispose();
-		headerImg.dispose();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.contribution.visualiser.views.VisualiserRendering#paintColumnHeader(org.eclipse.swt.graphics.GC,
-	 *      org.eclipse.contribution.visualiser.interfaces.IMember, int)
-	 */
-	public void paintColumnHeader(GC gc, IMember m, int x, int colWidth) {
-		int title_y_start = getMarginSize();
-		int title_y_height = getColumnHeaderHeight();
-
-		if ((header == null) || (header.width != colWidth)) {
-			createHeader(colWidth);
-		}
-		Image headerImg = new Image(Display.getDefault(), header);
-		gc.drawImage(headerImg, x, title_y_start);
-		headerImg.dispose();
-
-		String name = getConstrainedString(gc, m.getName(), colWidth - 4);
-		int xoff = (colWidth - gc.stringExtent(name).x) / 2;
-		gc.setForeground(ColorConstants.white);
-		gc.drawString(name, x + xoff, title_y_start + 4, true);
-		gc.setForeground(ColorConstants.black);
-
-		gc.setForeground(outlineColor);
-		gc.drawLine(x, title_y_start + cornerSize, x, title_y_start
-				+ title_y_height);
-		gc.drawLine(x + cornerSize, title_y_start, x + colWidth - cornerSize,
-				title_y_start);
-		gc.drawLine(x + colWidth, title_y_start + cornerSize, x + colWidth,
-				title_y_start + title_y_height);
-	}
 
 }
