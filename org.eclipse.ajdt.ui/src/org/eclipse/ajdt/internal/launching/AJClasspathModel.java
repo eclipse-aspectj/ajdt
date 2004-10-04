51bd8c160f903c0c3c9c5748a8cdc5539bc609b7
mchapman <mchapman>
2004-10-04 14:05:27 +0000

    first fix to add support for Eclipse 3.1M2

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/AJClasspathModel.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/AJClasspathModel.java
@@ -11,6 +11,8 @@
 
 package org.eclipse.ajdt.internal.launching;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 
 import org.eclipse.ajdt.ui.AspectJUIPlugin;
@@ -76,7 +78,8 @@ public class AJClasspathModel extends ClasspathModel {
 			}
 		}
 		if (entryParent != null) {
-			((ClasspathGroup) entryParent).addEntry(newEntry);
+			//((ClasspathGroup) entryParent).addEntry(newEntry);
+			addEntryToClasspathGroup((ClasspathGroup) entryParent,newEntry);
 		} else {
 			childEntries.add(newEntry);
 		}
@@ -107,7 +110,9 @@ public class AJClasspathModel extends ClasspathModel {
 		}
 		bootstrapEntries.removeAll();
 		for (int i = 0; i < entries.length; i++) {
-			bootstrapEntries.addEntry(new ClasspathEntry(entries[i],
+			//bootstrapEntries.addEntry(new ClasspathEntry(entries[i],
+			//		bootstrapEntries));
+			addEntryToClasspathGroup(bootstrapEntries,new ClasspathEntry(entries[i],
 					bootstrapEntries));
 		}
 	}
@@ -121,7 +126,8 @@ public class AJClasspathModel extends ClasspathModel {
 		}
 		userEntries.removeAll();
 		for (int i = 0; i < entries.length; i++) {
-			userEntries.addEntry(new ClasspathEntry(entries[i], userEntries));
+			//userEntries.addEntry(new ClasspathEntry(entries[i], userEntries));
+			addEntryToClasspathGroup(userEntries,new ClasspathEntry(entries[i], userEntries));
 		}
 	}
 
@@ -137,7 +143,9 @@ public class AJClasspathModel extends ClasspathModel {
 		}
 		aspectPathEntries.removeAll();
 		for (int i = 0; i < entries.length; i++) {
-			aspectPathEntries.addEntry(new ClasspathEntry(entries[i],
+			//aspectPathEntries.addEntry(new ClasspathEntry(entries[i],
+			//		aspectPathEntries));
+			addEntryToClasspathGroup(aspectPathEntries,new ClasspathEntry(entries[i],
 					aspectPathEntries));
 		}
 	}
@@ -181,7 +189,9 @@ public class AJClasspathModel extends ClasspathModel {
 				canBeRemoved);
 
 		for (int i = 0; i < entries.length; i++) {
-			group.addEntry(new ClasspathEntry(entries[i], group));
+			//group.addEntry(new ClasspathEntry(entries[i], group));
+			addEntryToClasspathGroup(group, new ClasspathEntry(entries[i],
+					group));
 		}
 
 		if (addEntry) {
@@ -216,4 +226,31 @@ public class AJClasspathModel extends ClasspathModel {
 		return new IClasspathEntry[0];
 	}
 
+	/**
+	 * Use this instead of calling group.addEntry(), so that we can use either the Eclipse 3.0 or
+	 * 3.1 version of this internal API (the 3.1 version has an extra parameter).
+	 * @param group
+	 * @param entry
+	 */
+	private void addEntryToClasspathGroup(ClasspathGroup group,
+			IClasspathEntry entry) {
+		Class clazz = group.getClass();
+		Method m;
+		try {
+			try {
+				m = clazz.getMethod("addEntry",
+						new Class[] { IClasspathEntry.class });
+				m.invoke(group, new Object[] { entry });
+			} catch (NoSuchMethodException e) {
+				m = clazz.getMethod("addEntry", new Class[] {
+						IClasspathEntry.class, Object.class });
+				m.invoke(group, new Object[] { entry, null });
+			}
+		} catch (SecurityException e) {
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalArgumentException e) {
+		} catch (IllegalAccessException e) {
+		} catch (InvocationTargetException e) {
+		}
+	}
 }
