0ebe51bd2aa5adec8ea549576e8173913ce0af78
mchapman <mchapman>
2004-11-03 10:42:24 +0000

    fix and test for bug 77261: Shouldn't be able to include files from outside a source folder.

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/buildconfigurator/popup/actions/IncludeAction.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/buildconfigurator/popup/actions/IncludeAction.java
@@ -19,11 +19,15 @@ import org.eclipse.ajdt.internal.ui.ajde.ProjectProperties;
 import org.eclipse.ajdt.ui.AspectJUIPlugin;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 
 /**
  * @author Luzius Meisser
@@ -60,12 +64,26 @@ public class IncludeAction extends BuildConfigurationChangeAction {
 				IFile file = (IFile) o;
 				if (ProjectProperties.ASPECTJ_SOURCE_FILTER.accept(file
 						.getName())) {
-
 					ProjectBuildConfigurator pbc = buildConfigurator
-							.getProjectBuildConfigurator(file.getProject());
-					if (pbc != null)
-						return !pbc.getActiveBuildConfiguration().isIncluded(
-								file);
+						.getProjectBuildConfigurator(file.getProject());
+					if ((pbc!=null) && pbc.getActiveBuildConfiguration().isIncluded(
+							file)) {
+						return false;
+					}
+					
+					// the file is not currently included but we only give the
+					// option to include it if it is contained in a source folder
+					IJavaProject jp = JavaCore.create(file.getProject());
+					IClasspathEntry[] cpes = jp.getRawClasspath();
+					IPath fp = file.getFullPath();
+					for (int i = 0; i < cpes.length; i++) {
+						if (cpes[i].getEntryKind() == IClasspathEntry.CPE_SOURCE) {
+							IPath p = cpes[i].getPath();
+							if (p.isPrefixOf(fp)) {
+								return true;
+							}
+						}
+					}
 				}
 			}
 		} catch (CoreException e) {
