a3dfd230e29bc30aba4df35bb5db3cee71bbe838
mchapman <mchapman>
2004-11-25 16:56:23 +0000

    another part of enhancement 78378: add java element filters for aj elements. these will apply to the package explorer and the standard java outline view, plus the in-place outline view

---
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/editor/outline/AdviceFilter.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Matt Chapman - initial version
+ *******************************************************************************/
+package org.eclipse.ajdt.internal.ui.editor.outline;
+
+import org.eclipse.ajdt.core.javaelements.AdviceElement;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+/**
+ * Filter to allow hiding of advice from package explorer, outline view etc
+ */
+public class AdviceFilter extends ViewerFilter {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ViewerFilter#select(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (element instanceof AdviceElement) {
+			return false;
+		}
+		return true;
+	}
+
+}
