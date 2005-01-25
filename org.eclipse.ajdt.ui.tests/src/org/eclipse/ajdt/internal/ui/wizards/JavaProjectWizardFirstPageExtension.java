aca188d7d0bf7618382418b77e7315665266f1a2
mchapman <mchapman>
2005-01-25 16:04:51 +0000

    update copyright

--- org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/internal/ui/wizards/JavaProjectWizardFirstPageExtension.java
+++ org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/internal/ui/wizards/JavaProjectWizardFirstPageExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,7 +48,8 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
 
 /**
- * Copied JavaProjectWizardFirstPage to enable access to private fields for testing
+ * Copied org.eclipse.jdt.internal.ui.wizards.JavaProjectWizardFirstPage to
+ * enable access to private fields for testing
  */
 public class JavaProjectWizardFirstPageExtension extends JavaProjectWizardFirstPage {
 
