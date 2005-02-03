01e965b292a8600bdf11f0efe9515e0921a2258a
mchapman <mchapman>
2005-02-03 11:10:10 +0000

    new test project for 84310

---
+++ org.eclipse.ajdt.ui.tests/workspace/bug84310b/src/foo/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 03-Feb-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package foo;
+
+/**
+ * @author mchapman
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+	}
+}
