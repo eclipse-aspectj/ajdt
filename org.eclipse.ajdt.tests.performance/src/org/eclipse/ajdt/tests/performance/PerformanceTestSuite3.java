0a5f84ab4d3d79d0a20206826783bf47973dbff1
mchapman <mchapman>
2005-03-09 17:10:06 +0000

    new performance tests plugin

--- org.eclipse.ajdt.tests.performance/src/org/eclipse/ajdt/tests/performance/PerformanceTestSuite3.java
+++ org.eclipse.ajdt.tests.performance/src/org/eclipse/ajdt/tests/performance/PerformanceTestSuite3.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,17 +17,13 @@ import junit.framework.TestSuite;
 /**
  * @since 3.1
  */
-public class PerformanceTestSuite extends TestSuite {
+public class PerformanceTestSuite3 extends TestSuite {
 
 	public static Test suite() {
-		return new PerformanceTestSetup(new PerformanceTestSuite());
+		return new PerformanceTestSetup(new PerformanceTestSuite3());
 	}
 	
-	public PerformanceTestSuite() {
-		// add new performance tests here:
-		
-		
-		addTest(new OpenJavaEditorTest.Setup(EmptyTestCase.suite(), false)); // the actual test runs in its own workbench (see test.xml)
-		addTest(new OpenAspectJEditorTest.Setup(EmptyTestCase.suite(), false)); // the actual test runs in its own workbench (see test.xml)
+	public PerformanceTestSuite3() {
+		addTest(OpenAspectJEditorTest.suite());
 	}
 }
