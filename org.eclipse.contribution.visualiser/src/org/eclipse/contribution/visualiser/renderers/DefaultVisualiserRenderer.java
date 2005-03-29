43b41d9cf315487536e6544778d447594cc0f9c4
mchapman <mchapman>
2005-03-29 07:10:00 +0000

    mac: rendering fix for the visualiser (thx mini mac!)

--- org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/renderers/DefaultVisualiserRenderer.java
+++ org.eclipse.contribution.visualiser/src/org/eclipse/contribution/visualiser/renderers/DefaultVisualiserRenderer.java
@@ -49,7 +49,7 @@ public class DefaultVisualiserRenderer extends ClassicVisualiserRenderer {
 	protected static int cornerSize = 6;
 
 	protected static byte[][] cornerData = new byte[][] { { 1, 5, 0, 5, 0 },
-			{ 0, 1, 1, 2, 1 }, { 1, 3, 1, 3, 1 }, { 2, 4, 1, 5, 1 },
+			{ 0, 1, 1, 1, 1 }, { 0, 2, 1, 2, 1}, { 1, 3, 1, 3, 1 }, { 2, 4, 1, 5, 1 },
 			{ 0, 1, 2, 1, 2 }, { 1, 2, 2, 2, 2 }, { 2, 3, 2, 3, 2 },
 			{ 1, 1, 3, 1, 3 }, { 2, 2, 3, 2, 3 }, { 1, 0, 4, 0, 5 },
 			{ 2, 1, 4, 1, 5 } };
