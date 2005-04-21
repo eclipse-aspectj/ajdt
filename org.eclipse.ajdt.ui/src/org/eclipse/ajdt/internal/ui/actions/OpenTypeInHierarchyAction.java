7bcc3c7301f58b521e27bad4af69c43806f07e0b
sjanuary <sjanuary>
2005-04-21 10:57:38 +0000

    New open type dialogs and actions

---
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/actions/OpenTypeInHierarchyAction.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: Sian January - initial version
+ ******************************************************************************/
+package org.eclipse.ajdt.internal.ui.actions;
+
+/**
+ * @author Sian
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class OpenTypeInHierarchyAction extends
+		org.eclipse.jdt.internal.ui.actions.OpenTypeInHierarchyAction {
+
+	/**
+	 * 
+	 */
+	public OpenTypeInHierarchyAction() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
