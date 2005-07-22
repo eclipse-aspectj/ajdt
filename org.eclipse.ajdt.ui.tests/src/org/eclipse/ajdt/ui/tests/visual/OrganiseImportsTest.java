f090da7c80b9bb8b8278564cc0d0e6e47a948bd6
sjanuary <sjanuary>
2005-07-22 12:48:18 +0000

    Organise imports visual tests

---
+++ org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/visual/OrganiseImportsTest.java
@@ -0,0 +1,167 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Sian January  - initial version
+ *******************************************************************************/
+package org.eclipse.ajdt.ui.tests.visual;
+
+import org.eclipse.ajdt.core.javaelements.AJCompilationUnitManager;
+import org.eclipse.ajdt.ui.tests.testutils.Utils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.swt.SWT;
+
+/**
+ * Visual test case for organise imports in a .aj file
+ */
+public class OrganiseImportsTest extends VisualTestCase {
+
+	
+	public void testOrganiseImports() throws Exception {
+		IProject project = Utils.createPredefinedProject("Bean Example");
+		assertTrue("The Bean Example project should have been created", project != null);
+		try {
+			IFile boundPoint = (IFile)project.findMember("src/bean/BoundPoint.aj");
+			assertTrue("The bean example project should contain a file called 'BoundPoint.aj'", boundPoint != null );
+			Utils.openFileInDefaultEditor(boundPoint, true);
+			final ICompilationUnit cUnit = AJCompilationUnitManager.INSTANCE.getAJCompilationUnit(boundPoint);
+			assertTrue("BoundPoint.aj should start with two imports", cUnit.getImports().length == 2);
+			Utils.waitForJobsToComplete();			
+			
+			// Organise imports and test that the file is correct
+			organiseImports(cUnit, 3);			
+			
+			// Add an unused import
+			gotoLine(19);
+			postString("import java.io.File;");
+			postCharacterKey(SWT.CR);
+			
+			// Post Ctrl+S to save the file
+			postKeyDown(SWT.CTRL);
+			postCharacterKey('s');
+			postKeyUp(SWT.CTRL);
+			
+			// Wait for the save to be processed
+			new DisplayHelper() {
+				protected boolean condition() {
+					try {					
+						IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+						return problemMarkers.length == 1;
+					} catch (CoreException e) {
+					}
+					return false;
+				}
+			
+			}.waitForCondition(display, 5000);
+
+			// Check that there is an unused import in the file
+			IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+			assertTrue("BoundPoint.aj should have one problem", problemMarkers.length == 1);
+			assertTrue("BoundPoint.aj should have four imports", cUnit.getImports().length == 4);
+			
+			organiseImports(cUnit, 3);
+
+			// Add an import requirement - pointcut p2(): call(* File.*(..));
+			gotoLine(65);
+			postCharacterKey(SWT.TAB);
+			postString("pointcut p2");
+			postKeyDown(SWT.SHIFT);
+			postString("90; ");
+			postKeyUp(SWT.SHIFT);
+			postString("call");
+			postKeyDown(SWT.SHIFT);
+			postString("98 ");
+			postKeyUp(SWT.SHIFT);
+			postString("File.");
+			postKeyDown(SWT.SHIFT);
+			postString("89");
+			postKeyUp(SWT.SHIFT);
+			postString("..");
+			postKeyDown(SWT.SHIFT);
+			postString("00");
+			postKeyUp(SWT.SHIFT);	
+			postString(";");		
+			postCharacterKey(SWT.DEL); // delete the extra bracket eclipse added
+			postCharacterKey(SWT.CR);			
+			
+			// Post Ctrl+S to save the file
+			postKeyDown(SWT.CTRL);
+			postCharacterKey('s');
+			postKeyUp(SWT.CTRL);
+			
+			// Wait for the save to be processed
+			new DisplayHelper() {
+				protected boolean condition() {
+					try {					
+						IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+						return problemMarkers.length == 1;
+					} catch (CoreException e) {
+					}
+					return false;
+				}
+			
+			}.waitForCondition(display, 5000);
+			
+			organiseImports(cUnit, 4);
+			
+		} finally {
+			Utils.waitForJobsToComplete();
+			Utils.deleteProject(project);
+		}
+	}
+
+	private void organiseImports(final ICompilationUnit cUnit, final int expectedImports) throws JavaModelException, CoreException {
+		// Post Ctrl+Shift+O to organise imports
+		postKeyDown(SWT.CTRL);
+		postKeyDown(SWT.SHIFT);
+		postCharacterKey('o');
+		postKeyUp(SWT.SHIFT);
+		postKeyUp(SWT.CTRL);
+
+		// Wait for the save to be processed
+		new DisplayHelper() {
+			protected boolean condition() {
+				try {
+					return  cUnit.getImports().length == expectedImports;
+				} catch (JavaModelException e) {
+				}
+				return false;
+			}
+		
+		}.waitForCondition(display, 5000);
+		assertTrue("BoundPoint.aj should now have " + expectedImports + " imports, has " + cUnit.getImports().length, cUnit.getImports().length == expectedImports);
+		
+		// Post Ctrl+S to save the file
+		postKeyDown(SWT.CTRL);
+		postCharacterKey('s');
+		postKeyUp(SWT.CTRL);
+
+		// Wait for the build to remove any problem markers
+		new DisplayHelper() {
+			protected boolean condition() {
+				try {					
+					IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+					return problemMarkers.length == 0;
+				} catch (CoreException e) {
+				}
+				return false;
+			}
+		
+		}.waitForCondition(display, 5000);
+		IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+		assertTrue("BoundPoint.aj should not have any problems", problemMarkers.length == 0);
+	
+	}
+	
+}
---
+++ org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/visual/OrganiseImportsTest2.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Sian January  - initial version
+ *******************************************************************************/
+package org.eclipse.ajdt.ui.tests.visual;
+
+import org.eclipse.ajdt.core.javaelements.AJCompilationUnitManager;
+import org.eclipse.ajdt.ui.tests.testutils.Utils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.swt.SWT;
+
+/**
+ * A second visual test case for organise imports in a .aj file
+ */
+public class OrganiseImportsTest2 extends VisualTestCase {
+
+	/**
+	 * Test organize imports when there's nothing to add or remove
+	 * in an aspect that extends an abstract aspect and has more 
+	 * than one variable.
+	 * @throws Exception
+	 */
+	public void testOrganiseImports2() throws Exception {
+		IProject project = Utils.createPredefinedProject("Spacewar Example");
+		try {
+			IFile gameSynchronizationFile = (IFile)project.findMember("src/spacewar/GameSynchronization.aj");
+			assertTrue("The bean example project should contain a file called 'GameSynchronization.aj'", gameSynchronizationFile != null );
+			Utils.openFileInDefaultEditor(gameSynchronizationFile, true);
+			final ICompilationUnit cUnit = AJCompilationUnitManager.INSTANCE.getAJCompilationUnit(gameSynchronizationFile);
+			assertTrue("GameSynchronization.aj should start with one imports", cUnit.getImports().length == 1);
+			Utils.waitForJobsToComplete();			
+			
+//			 Post Ctrl+Shift+O to organise imports
+			postKeyDown(SWT.CTRL);
+			postKeyDown(SWT.SHIFT);
+			postCharacterKey('o');
+			postKeyUp(SWT.SHIFT);
+			postKeyUp(SWT.CTRL);
+
+//			 Wait for an error condition to make sure it's caught if it does occur
+			new DisplayHelper() {
+				protected boolean condition() {
+					try {
+						return  cUnit.getImports().length == 0;
+					} catch (JavaModelException e) {
+					}
+					return false;
+				}
+			
+			}.waitForCondition(display, 3000);
+			assertTrue("BoundPoint.aj should now have one imports, has " + cUnit.getImports().length, cUnit.getImports().length == 1);
+			
+			// Post Ctrl+S to save the file
+			postKeyDown(SWT.CTRL);
+			postCharacterKey('s');
+			postKeyUp(SWT.CTRL);
+
+			// Wait for an error condition to make sure it's caught if it does occur
+			new DisplayHelper() {
+				protected boolean condition() {
+					try {					
+						IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+						return problemMarkers.length == 1;
+					} catch (CoreException e) {
+					}
+					return false;
+				}
+			
+			}.waitForCondition(display, 3000);
+			IMarker[] problemMarkers = cUnit.getResource().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_ONE);
+			assertTrue("BoundPoint.aj should not have any problems", problemMarkers.length == 0);
+
+		} finally {
+			Utils.waitForJobsToComplete();
+			Utils.deleteProject(project);
+		}
+	}
+
+	
+}
