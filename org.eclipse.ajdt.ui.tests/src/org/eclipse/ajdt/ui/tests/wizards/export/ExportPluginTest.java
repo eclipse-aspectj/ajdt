eb443aea873c8733916718a871d6592b5da9c649
mchapman <mchapman>
2005-09-21 16:17:36 +0000

    removed unused import

--- org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/wizards/export/ExportPluginTest.java
+++ org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/wizards/export/ExportPluginTest.java
@@ -13,7 +13,6 @@ package org.eclipse.ajdt.ui.tests.wizards.export;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
