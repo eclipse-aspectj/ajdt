1d80653b1c35805d590ae44d94021fa1f77013fc
sjanuary <sjanuary>
2005-11-01 16:20:13 +0000

    organised imports

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/LTWUtils.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/LTWUtils.java
@@ -34,7 +34,6 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
