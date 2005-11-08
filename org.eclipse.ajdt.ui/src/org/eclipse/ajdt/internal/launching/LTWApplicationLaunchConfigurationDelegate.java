712c1511cf3f67cd46a94a4c22b8f4bd2a46c06d
sjanuary <sjanuary>
2005-11-08 12:15:05 +0000

    Fix for LTW problem on the Mac

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/LTWApplicationLaunchConfigurationDelegate.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/launching/LTWApplicationLaunchConfigurationDelegate.java
@@ -32,6 +32,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
