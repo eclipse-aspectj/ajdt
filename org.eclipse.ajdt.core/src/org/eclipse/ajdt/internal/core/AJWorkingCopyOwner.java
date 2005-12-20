f099d2851c7a955fffb6104ec1abf81fedd93d3d
sjanuary <sjanuary>
2005-12-20 09:26:03 +0000

    Ported the fix for 117412 to HEAD

---
+++ org.eclipse.ajdt.core/src/org/eclipse/ajdt/internal/core/AJWorkingCopyOwner.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ *
+ * Contributors:
+ *     Sian January - initial version
+ *******************************************************************************/
+package org.eclipse.ajdt.internal.core;
+
+import org.eclipse.jdt.core.IBuffer;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.WorkingCopyOwner;
+import org.eclipse.jdt.internal.core.DefaultWorkingCopyOwner;
+
+/**
+ * A working copy owner that creates internal buffers.
+ * Based on org.eclipse.jdt.internal.core.DefaultWorkingCopyOwner. 
+ * Part of the fix for 117412.
+ */
+public class AJWorkingCopyOwner extends WorkingCopyOwner {
+	
+		
+	public static final AJWorkingCopyOwner INSTANCE =  new AJWorkingCopyOwner();
+	
+	private AJWorkingCopyOwner() {
+		// singleton - so use a private constructor
+	}
+
+	public IBuffer createBuffer(ICompilationUnit workingCopy) {
+		// AspectJ Change begin - use DefaultWorkingCopyOwner.PRIMARY.primaryBufferProvider because that has been initialized, wheras a local version wouldn't be
+		if (DefaultWorkingCopyOwner.PRIMARY.primaryBufferProvider != null) return DefaultWorkingCopyOwner.PRIMARY.primaryBufferProvider.createBuffer(workingCopy);
+		return super.createBuffer(workingCopy);
+	}
+	
+	public String toString() {
+		return "AJDT working copy owner"; //$NON-NLS-1$
+	}
+}
