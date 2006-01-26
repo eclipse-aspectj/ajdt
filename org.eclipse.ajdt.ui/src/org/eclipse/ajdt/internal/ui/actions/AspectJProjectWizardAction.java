c16b79367fd7d9fa1a0f78cfddd2cf73455e5833
mchapman <mchapman>
2006-01-26 12:23:34 +0000

    125171: New Project wizard in AspectJ cheatsheet doesn't work

---
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/actions/AspectJProjectWizardAction.java
@@ -0,0 +1,28 @@
+/**********************************************************************
+Copyright (c) 2002, 2006 IBM Corporation and others.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+Contributors:
+           Adrian Colyer - initial version
+
+**********************************************************************/
+package org.eclipse.ajdt.internal.ui.actions;
+
+import org.eclipse.ajdt.internal.ui.wizards.AspectJProjectWizard;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.ui.actions.AbstractOpenWizardAction;
+import org.eclipse.jface.wizard.Wizard;
+
+/**
+ * This action is called from the AJDT welcome page and cheatsheet
+ * to launch the new aspectj project wizard.
+ */
+public class AspectJProjectWizardAction extends AbstractOpenWizardAction {
+
+	protected Wizard createWizard() throws CoreException {
+		return new AspectJProjectWizard();
+	}
+
+}
