9e1934ea044f61768e9c580eba0873a32c00a11b
mchapman <mchapman>
2006-02-21 14:56:37 +0000

    125021: Inherited abstract pointcut stubs aren't created in new aspect wizard

--- org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/javaelements/AJCompilationUnitManager.java
+++ org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/javaelements/AJCompilationUnitManager.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import org.eclipse.ajdt.core.AspectJPlugin;
 import org.eclipse.ajdt.core.CoreUtils;
+import org.eclipse.ajdt.internal.core.AJWorkingCopyOwner;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -35,6 +36,7 @@ import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.WorkingCopyOwner;
 import org.eclipse.jdt.internal.core.JavaElement;
 import org.eclipse.jdt.internal.core.JavaModelManager;
 import org.eclipse.jdt.internal.core.OpenableElementInfo;
@@ -74,6 +76,9 @@ public class AJCompilationUnitManager {
 		if (AspectJPlugin.usingCUprovider) { // mapping not required
 			return cu;
 		}
+		if (cu == null) {
+			return cu;
+		}
 		IResource res = cu.getResource();
 		if (res.getType() == IResource.FILE) {
 			AJCompilationUnit ajcu = INSTANCE.getAJCompilationUnit((IFile) res);
@@ -82,6 +87,14 @@ public class AJCompilationUnitManager {
 			}
 		}
 		return cu;
+	}
+	
+	/**
+	 * Returns the WorkingCopyOwner used to create AJCompilationUnits
+	 * @return
+	 */
+	public static WorkingCopyOwner defaultAJWorkingCopyOwner() {
+		return AJWorkingCopyOwner.INSTANCE;
 	}
 	
 	//returns true if it was already there, and false if it needed to be inserted
