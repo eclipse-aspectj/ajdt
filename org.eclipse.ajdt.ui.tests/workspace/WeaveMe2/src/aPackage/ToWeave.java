7403c9bfd83fd5dfae85a89b81b117a2dd7e92c0
mchapman <mchapman>
2006-09-05 15:08:11 +0000

    visual tests for 154857

---
+++ org.eclipse.ajdt.ui.tests/workspace/WeaveMe2/src/aPackage/ToWeave.java
@@ -0,0 +1,15 @@
+package aPackage;
+
+public class ToWeave {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//should have before aspect
+		System.out.println("Main method called. WeaveMe2");
+
+	}
+
+}
