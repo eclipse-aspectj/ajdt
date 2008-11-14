15c3de5f3203db87a5270df01829c2a0e29f212d
aeisenberg <aeisenberg>
2008-11-14 05:27:54 +0000

    Moved over problem finder tests and code completion tests to UI because they weren't passing on the server when in core.

--- org.eclipse.ajdt.core.tests/src/org/eclipse/ajdt/core/tests/AllCoreTests.java
+++ org.eclipse.ajdt.core.tests/src/org/eclipse/ajdt/core/tests/AllCoreTests.java
@@ -26,7 +26,6 @@ import org.eclipse.ajdt.core.tests.builder.Bug99133Test;
 import org.eclipse.ajdt.core.tests.builder.CoreOutputLocationManagerTest;
 import org.eclipse.ajdt.core.tests.codeconversion.AspectsConvertingParserTest;
 import org.eclipse.ajdt.core.tests.codeconversion.CodeCheckerTest;
-import org.eclipse.ajdt.core.tests.contentassist.ContentAssistTests;
 import org.eclipse.ajdt.core.tests.dom.rewrite.ASTRewritingPointcutDeclTest;
 import org.eclipse.ajdt.core.tests.javaelements.AspectElementTests;
 import org.eclipse.ajdt.core.tests.model.AJCodeElementTest;
@@ -40,8 +39,6 @@ import org.eclipse.ajdt.core.tests.model.AJProjectModelTest;
 import org.eclipse.ajdt.core.tests.model.AJProjectModelTest2;
 import org.eclipse.ajdt.core.tests.model.AJRelationshipManagerTest;
 import org.eclipse.ajdt.core.tests.model.BinaryWeavingSupportTest;
-import org.eclipse.ajdt.core.tests.reconciling.ProblemFinderTests;
-import org.eclipse.ajdt.core.tests.reconciling.ProblemFinderTests2;
 import org.eclipse.ajdt.core.tests.refactoring.AspectRenameParticipantTest;
 
 /**
@@ -106,12 +103,6 @@ public class AllCoreTests {
 		// refactoring tests
 		suite.addTest(new TestSuite(AspectRenameParticipantTest.class));
 		
-		// reconciling and eager parsing tests
-		// these tests work on my local machine but don't work on the server.
-		// commenting out for now
-//        suite.addTest(new TestSuite(ProblemFinderTests.class));
-//        suite.addTest(new TestSuite(ProblemFinderTests2.class));
-		suite.addTest(new TestSuite(ContentAssistTests.class));
 		return suite;
 	}
 }
--- org.eclipse.ajdt.core.tests/workspace/ITDTesting/src/test/OtherClass.aj
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class OtherClass {
-    void x() {
-        Demo d = new Demo(4);
-        d.foo(null);
-        d.x ++;
-    }
-}
