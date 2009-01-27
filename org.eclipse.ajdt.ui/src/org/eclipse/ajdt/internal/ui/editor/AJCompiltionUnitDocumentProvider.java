a77fba42f3a9037efadf2a9364f8ab352e053d7b
aeisenberg <aeisenberg>
2009-01-27 00:23:44 +0000

    Not Using AJCompilationUnitDocumentProvider.  Deprecated.

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/editor/AJCompiltionUnitDocumentProvider.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/editor/AJCompiltionUnitDocumentProvider.java
@@ -5,6 +5,10 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitDocumentProvider;
 
+/**
+ * 
+ * @deprecated Not used any more.  Use {@link CompilationUnitDocumentProvider} instead
+ */
 public class AJCompiltionUnitDocumentProvider extends
         CompilationUnitDocumentProvider {
 
