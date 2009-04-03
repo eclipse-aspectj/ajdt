2d449a500b0744aef71746cdc2a84babf8e5d4f6
aeisenberg <aeisenberg>
2009-04-03 22:23:59 +0000

    avoid possible NPE as stated in bug 270123

--- org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/parserbridge/ITDInserter.java
+++ org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/parserbridge/ITDInserter.java
@@ -254,11 +254,16 @@ public class ITDInserter extends ASTVisitor {
                 }
                 sig = new ErasedTypeSignature(method.getCorrespondingType(true), params);
             }
-            for (int i = 0; i < args.length; i++) {
-                args[i] = new Argument(((String) method.getParameterNames().get(i)).toCharArray(),
-                        0,
-                        createTypeReference(Signature.getElementType(sig.paramTypes[i])),
-                        0);
+            
+            // possibly avoids npe in bug 270123
+            List/*String*/ pNames = method.getParameterNames();
+            if (pNames != null) {
+                for (int i = 0; i < args.length; i++) {
+                    args[i] = new Argument(((String) pNames.get(i)).toCharArray(),
+                            0,
+                            createTypeReference(Signature.getElementType(sig.paramTypes[i])),
+                            0);
+                }
             }
         } catch (Exception e) {
             for (int i = 0; i < args.length; i++) {
