a8070023c7c77480cfe155ddde48b2a46394356c
aeisenberg <aeisenberg>
2009-05-21 20:38:21 +0000

    Remove check box from table.

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/refactoring/PushInRefactoringInputPage.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/refactoring/PushInRefactoringInputPage.java
@@ -12,9 +12,9 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.ui.util.SWTUtil;
 import org.eclipse.jdt.internal.ui.util.TableLayoutComposite;
 import org.eclipse.jface.viewers.CellLabelProvider;
-import org.eclipse.jface.viewers.CheckboxTableViewer;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerCell;
@@ -45,10 +45,6 @@ public class PushInRefactoringInputPage extends UserInputWizardPage {
 		GridLayout layout= new GridLayout(2, false);
 		result.setLayout(layout);
 
-//		Label label = new Label(result, SWT.NONE);
-//		label.setText("&ITD name:");
-//		createNameField(result);
-
         Label label = new Label(result, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
         label.setText("Select ITDs to push in");
         GridData gridData= new GridData(GridData.FILL_HORIZONTAL);
@@ -56,51 +52,10 @@ public class PushInRefactoringInputPage extends UserInputWizardPage {
         gridData.verticalIndent= 5;
         label.setLayoutData(gridData);
 
-		
 		createTable(result);
-//		
-//        label = new Label(result, SWT.NONE);
-//        label.setText("&Affects taget types:");
-//        createAffectsTypesField(result);
-//        
-//        label = new Label(result, SWT.NONE);
-//        label.setText("&Declaring Aspect:");
-//        createDeclaringAspectField(result);
-
         handleInputChanged();
 	}
 
-//	private Label createNameField(Composite result) {
-//        Label field = new Label(result, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-//        field.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-//        List itds = getPushInRefactoring().getITDs();
-//        StringBuffer sb = new StringBuffer();
-//        for (Iterator itdIter = itds.iterator(); itdIter.hasNext();) {
-//            IntertypeElement itd = (IntertypeElement) itdIter.next();
-//            sb.append(itd.getElementName() + "\n");
-//        }
-//        field.setText(sb.toString());
-//        return field;
-//    }
-//	private Label createAffectsTypesField(Composite result) {
-//        Label field = new Label(result, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-//        field.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-//        IntertypeElement itd = getPushInRefactoring().getITD();
-//        try {
-//            field.setText(new String( itd.getTargetType()));
-//        } catch (JavaModelException e) {
-//            field.setText("Unknown");
-//        }
-//        return field;
-//    }
-//    private Label createDeclaringAspectField(Composite result) {
-//        Label field = new Label(result, SWT.SINGLE | SWT.LEFT | SWT.BORDER);
-//        field.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-//        IntertypeElement itd = getPushInRefactoring().getITD();
-//        field.setText(itd.getParent().getElementName());
-//        return field;
-//    }
-    
 
 
 	private PushInRefactoring getPushInRefactoring() {
@@ -130,21 +85,16 @@ public class PushInRefactoringInputPage extends UserInputWizardPage {
 	 * from @link ExtractClassUserInputWizardPage
 	 */
     private void createTable(Composite parent) {
-//        Composite result= new Composite(parent, SWT.NONE);
-//        GridLayout layout= new GridLayout(2, false);
-//        layout.marginHeight= 0;
-//        layout.marginWidth= 0;
-//        result.setLayout(layout);
-        GridData gridData; //= new GridData(GridData.FILL_BOTH);
-//        gridData.horizontalSpan= 2;
-//        result.setLayoutData(gridData);
-
+        GridData gridData;
 
         TableLayoutComposite layoutComposite= new TableLayoutComposite(parent, SWT.NONE);
         layoutComposite.addColumnData(new ColumnWeightData(40, convertWidthInCharsToPixels(20), true));
         layoutComposite.addColumnData(new ColumnWeightData(40, convertWidthInCharsToPixels(20), true));
         layoutComposite.addColumnData(new ColumnWeightData(40, convertWidthInCharsToPixels(20), true));
-        final CheckboxTableViewer tv= CheckboxTableViewer.newCheckList(layoutComposite, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+        
+        // use this instead if we want to implement check boxes
+//        final CheckboxTableViewer tv = CheckboxTableViewer.newCheckList(layoutComposite, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+        final TableViewer tv = new TableViewer(layoutComposite, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
         tv.setContentProvider(new FieldContentProvider());
         createColumns(tv);
 
@@ -155,17 +105,18 @@ public class PushInRefactoringInputPage extends UserInputWizardPage {
         table.setLayoutData(gridData);
         tv.setInput(descriptor);
         final Object[] fields= descriptor.getITDs();
-        for (int i= 0; i < fields.length; i++) {
-            IAspectJElement field= (IAspectJElement) fields[i];
-            tv.setChecked(field, true);
-        }
+        // only if using checkbox viewer
+//        for (int i= 0; i < fields.length; i++) {
+//            IAspectJElement field= (IAspectJElement) fields[i];
+//            tv.setChecked(field, true);
+//        }
         tv.refresh(true);
         gridData= new GridData(GridData.FILL_BOTH);
         gridData.heightHint= SWTUtil.getTableHeightHint(table, Math.max(fields.length,5));
         layoutComposite.setLayoutData(gridData);
     }
     
-    private void createColumns(final CheckboxTableViewer tv) {
+    private void createColumns(final TableViewer tv) {
 
         // three columns: Aspect name, ITD name, target type name(s)
         
