e5ae7fd964edb3ce9d24ff11e166237ece7a239a
aeisenberg <aeisenberg>
2010-04-19 18:38:23 +0000

    ITD Aware search...now working inside of ITDs.

--- org.eclipse.ajdt.core.tests/src/org/eclipse/ajdt/core/tests/search/ITDAwareSearchTests.java
+++ org.eclipse.ajdt.core.tests/src/org/eclipse/ajdt/core/tests/search/ITDAwareSearchTests.java
@@ -11,6 +11,7 @@
 package org.eclipse.ajdt.core.tests.search;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.ajdt.core.javaelements.AJCompilationUnit;
@@ -22,6 +23,7 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
@@ -185,27 +187,106 @@ public class ITDAwareSearchTests extends AJDTCoreTestCase {
         assertMatch("new Java(7)", contents, matches);
     }
 
-    // disabled because we cannot do matching inside ITDs
-    public void _testITDSearchFieldInITD() throws Exception {
-        ICompilationUnit unit = createCU("Aspect.aj", "aspect Aspect { int Java.xxx = 0; \nvoid Java.foo() { xxx++; } }");
-        String contents = "class Java {  }";
-        createCU("Java.java", contents);
+    public void testITDSearchFieldInITD() throws Exception {
+        String contents = "aspect Aspect { void Java.foo() { xxx++; }\n int Java.xxx = 0; }";
+        ICompilationUnit unit = createCU("Aspect.aj", contents);
+        createCU("Java.java", "class Java {  }");
         
-        IntertypeElement itd = findFirstITD(unit);
+        IntertypeElement itd = findLastITD(unit);
         
         List matches = findSearchMatches(itd);
         assertMatch("xxx", contents, matches);
     }
-    // disabled because we cannot do matching inside ITDs
-    public void _testITDSearchMethodInITD() throws Exception {
+    
+    public void testITDSearchMethodInITD() throws Exception {
         String contents = "aspect Aspect { \nvoid Java.foo() { xxx(); }\n int Java.xxx() { return  0; } }";
         ICompilationUnit unit = createCU("Aspect.aj", contents);
         createCU("Java.java", "class Java {  }");
         
-        IntertypeElement itd = findFirstITD(unit);
+        IntertypeElement itd = findLastITD(unit);
         
         List matches = findSearchMatches(itd);
-        assertMatch("xxx", contents, matches);
+        assertMatch("xxx()", contents, matches);
+    }
+    public void testITDSearchRegularMethodInITD() throws Exception {
+        String contents = "aspect Aspect { \nvoid Java.foo() { regular(); } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { void regular() { } }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertMatch("regular()", contents, matches);
+    }
+    public void testITDSearchRegularMethodInITD2() throws Exception {
+        String contents = "aspect Aspect { \nvoid Java.foo() { regular(); regular(7); } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { void regular() { } void regular(int a) { } }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertMatch("regular()", contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD() throws Exception {
+        String contents = "aspect Aspect { \nvoid Java.foo() { regular++; } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertMatch("regular", contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD2() throws Exception {
+        String contents = "aspect Aspect { void Java.foo() { regular++; } int regular; }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertMatch("regular", contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD3() throws Exception {
+        String contents = "aspect Aspect { int regular;\n void Java.foo(int regular) { regular++; } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertNoMatch(contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD4() throws Exception {
+        String contents = "aspect Aspect { int regular;\n void Java.foo() { int regular = 0; regular++; } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertNoMatch(contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD5() throws Exception {
+        String contents = "aspect Aspect { int regular;\n void Java.foo() { for(int regular = 0; regular < 5; regular++) regular++; } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertNoMatch(contents, matches);
+    }
+    public void testITDSearchRegularFieldInITD6() throws Exception {
+        String contents = "aspect Aspect { int regular;\n void Java.foo() { int regular = 0, other = 0; regular++; } }";
+        createCU("Aspect.aj", contents);
+        ICompilationUnit unit = createCU("Java.java", "class Java { int regular; }");
+        
+        IMember toSearch = findFirstChild(unit);
+        
+        List matches = findSearchMatches(toSearch);
+        assertNoMatch(contents, matches);
     }
     public void testITDSearchConstructorInITD() throws Exception {
         String contents = "aspect Aspect { Java.new(int x) { this(); } \nvoid Java.foo() { new Java(7); } }";
@@ -229,8 +310,26 @@ public class ITDAwareSearchTests extends AJDTCoreTestCase {
         }
         return null;
     }
+    private IntertypeElement findLastITD(ICompilationUnit unit) throws Exception {
+        AJCompilationUnit ajUnit = (AJCompilationUnit) unit;
+        IType type = ajUnit.getTypes()[0];
+        IJavaElement[] children = type.getChildren();
+        for (int i = children.length-1; i >= 0 ; i--) {
+            if (children[i] instanceof IntertypeElement) {
+                return (IntertypeElement) children[i];
+            }
+        }
+        return null;
+    }
 
 
+    private IMember findFirstChild(ICompilationUnit unit) throws Exception {
+        IType type = unit.getTypes()[0];
+        IJavaElement[] children = type.getChildren();
+        return (IMember) children[0];
+    }
+    
+    
     private List findSearchMatches(IJavaElement elt) throws Exception {
         javaProject.getProject().build(IncrementalProjectBuilder.FULL_BUILD, null);
         waitForManualBuild();
@@ -252,9 +351,23 @@ public class ITDAwareSearchTests extends AJDTCoreTestCase {
         return createCompilationUnitAndPackage("", name, contents, javaProject);
     }
 
+    private void assertNoMatch(String contents, List matches) {
+        assertEquals("Should not have found any matches, but instead found matches in:\n" + contents + "\n\nMatches were:" + printMatches(matches), 0, matches.size());
+    }
+    
+    private String printMatches(List matches) {
+        StringBuffer sb = new StringBuffer();
+        for (Iterator matchIter = matches.iterator(); matchIter.hasNext();) {
+            SearchMatch match = (SearchMatch) matchIter.next();
+            sb.append("\n\n" + match);
+            
+        }
+        return sb.toString();
+    }
+
 
     private void assertMatch(String matchName, String contents, List matches) {
-        assertEquals("Should have found exactly 1 match", 1, matches.size());
+        assertEquals("Should have found exactly 1 match, but instead found " + printMatches(matches), 1, matches.size());
         SearchMatch match = (SearchMatch) matches.get(0);
         assertEquals("Wrong match location", contents.indexOf(matchName)+1, match.getOffset());
         assertEquals("Wrong match length", matchName.length(), match.getLength());
