a6ed5e7c23322fa8c382276d5836e62799d9321e
aeisenberg <aeisenberg>
2010-04-20 20:17:14 +0000

    Sync with 36 branch (head)

--- org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/AJSourceTransformer.java
+++ org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/AJSourceTransformer.java
@@ -1,9 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2009 SpringSource and others
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Andrew Eisenberg - initial version
+ *******************************************************************************/
 package org.eclipse.ajdt.core;
 
 import org.eclipse.ajdt.core.codeconversion.AspectsConvertingParser;
 import org.eclipse.ajdt.core.codeconversion.ConversionOptions;
 import org.eclipse.ajdt.core.codeconversion.JavaCompatibleBuffer;
-import org.eclipse.ajdt.core.javaelements.AJCompilationUnit;
 import org.eclipse.contribution.jdt.sourceprovider.ISourceTransformer;
 import org.eclipse.jdt.core.IBuffer;
 import org.eclipse.jdt.core.ICompilationUnit;
