9215833c260e7c1bc493c1be7f5afaa2ebbc35c4
aeisenberg <aeisenberg>
2010-06-28 17:40:04 +0000

    Try to fix tests failing on build server Also fix for Bug 318510

--- org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/ajde/UIMessageHandler.java
+++ org.eclipse.ajdt.ui/src/org/eclipse/ajdt/internal/ui/ajde/UIMessageHandler.java
@@ -49,6 +49,7 @@ import org.eclipse.jdt.core.IJavaModelMarker;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.omg.CORBA.MARSHAL;
 
 /**
  * IBuildMessageHandler implementation which records warnings in the Problems
@@ -538,6 +539,8 @@ public class UIMessageHandler implements IBuildMessageHandler {
 
     }
     
+    
+    private final static int MAX_MESSAGE_LENGTH = 2 ^ 16;
     /**
      * Sets the given marker to have the appropriate message.
      * 
@@ -555,6 +558,9 @@ public class UIMessageHandler implements IBuildMessageHandler {
      */
     private void setMessage(IMarker marker, String message)
             throws CoreException {
+        if (message == null) {
+            return;
+        }
         // FIXME: Remove this horrid hack.
         // Hack the filename off the front and the line number
         // off the end
@@ -563,6 +569,12 @@ public class UIMessageHandler implements IBuildMessageHandler {
                     .substring(message.indexOf("\":") + 2); //$NON-NLS-1$
             message = hackedMessage.substring(0, hackedMessage
                     .indexOf(", at line")); //$NON-NLS-1$
+        }
+        // bug 318150
+        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=318150
+        // Can't have more than 2^16 chars in a message
+        if (message.length() >= MAX_MESSAGE_LENGTH) {
+            message = message.substring(0, MAX_MESSAGE_LENGTH-1);
         }
         marker.setAttribute(IMarker.MESSAGE, message);
     }
