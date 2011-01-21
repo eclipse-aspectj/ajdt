62b6004d4407f28fba23cdf6729fa6592fb4bfa4
aeisenberg <aeisenberg>
2011-01-21 15:33:16 +0000

    Bug 334401 AJCompilationUnitStructureRequestor: Add annotations to Java model.

--- org.eclipse.contribution.weaving.jdt/src/org/eclipse/contribution/jdt/preferences/WeavingStateConfigurer.java
+++ org.eclipse.contribution.weaving.jdt/src/org/eclipse/contribution/jdt/preferences/WeavingStateConfigurer.java
@@ -19,7 +19,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.eclipse.contribution.jdt.IsWovenTester;
