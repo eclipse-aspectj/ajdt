57c5d2505f071bd5563cd72adcf9bbbc12dc8765
aeisenberg <aeisenberg>
2011-04-28 20:24:02 +0000

    Fix for Bug 34001 Some work on Bug 344159

--- org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/reconciling/GenericProblemFinderTests.java
+++ org.eclipse.ajdt.ui.tests/src/org/eclipse/ajdt/ui/tests/reconciling/GenericProblemFinderTests.java
@@ -93,4 +93,152 @@ public class GenericProblemFinderTests extends AbstractProblemFindingTests {
                         "package p;\n" +
                         "class Class { }" });
     }
+    
+    // test type parameters on method
+    // aspect and class in same file
+    public void testBug343001a() throws Exception {
+        assertNoProblems(
+                new String[] { "p" }, 
+                new String[] { "BadClass.aj" }, 
+                new String[] {
+                        "package p;\n" +
+                		"import java.util.List;\n" + 
+                		"import java.util.ArrayList;\n" + 
+                		"public class BadClass {\n" + 
+                		"    void g() {\n" + 
+                		"        List<Comparable<?>> v1 = persist();\n" + 
+                		"        ArrayList<Comparable<?>> v2 = this.persist();\n" + 
+                		"        String v3 = persist2();\n" + 
+                		"        BadClass v4 = this.persist2();\n" + 
+                		"        System.out.println(\"\"+v1+v2+v3+v4);\n" + 
+                		"    } \n" + 
+                		"}\n" + 
+                		"aspect Foo {\n" + 
+                		"    public <T extends List<Comparable<?>>>  T BadClass.persist() {\n" + 
+                		"        return null;\n" + 
+                		"    }\n" + 
+                		"    public <T> T BadClass.persist2() {\n" + 
+                		"        return null;\n" + 
+                		"    }\n" + 
+                		"}" });
+    }
+    
+    // test type parameters on method
+    // aspect and class in different file
+    public void testBug343001b() throws Exception {
+        assertNoProblems(
+                new String[] { "p", "q" }, 
+                new String[] { "BadClass.java", "Foo.aj" }, 
+                new String[] {
+                        "package p;\n" +
+                        "import java.util.List;\n" + 
+                        "import java.util.ArrayList;\n" + 
+                        "public class BadClass {\n" + 
+                        "    void g() {\n" + 
+                        "        List<Comparable<?>> v1 = persist();\n" + 
+                        "        ArrayList<Comparable<?>> v2 = this.persist();\n" + 
+                        "        String v3 = persist2();\n" + 
+                        "        BadClass v4 = this.persist2();\n" + 
+                        "        System.out.println(\"\"+v1+v2+v3+v4);\n" + 
+                        "    } \n" + 
+                        "}",
+                        
+                        "package q;\n" +
+                        "import java.util.List;\n" + 
+                        "import p.BadClass;" +
+                        "aspect Foo {\n" + 
+                        "    public <T extends List<Comparable<?>>>  T BadClass.persist() {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "    public <T> T BadClass.persist2() {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "}" });
+    }
+    
+    // test type parameters on method
+    // aspect and class in different file and references in different file
+    public void testBug343001c() throws Exception {
+        assertNoProblems(
+                new String[] { "p", "q", "r" }, 
+                new String[] { "BadClass.java", "Foo.aj", "Other.java" }, 
+                new String[] {
+                        "package p;\n" +
+                        "public class BadClass {\n" + 
+                        "}",
+                        
+                        "package q;\n" +
+                        "import java.util.List;\n" + 
+                        "import p.BadClass;\n" +
+                        "aspect Foo {\n" + 
+                        "    public <T extends List<Comparable<?>>> T BadClass.persist() {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "    public <T> T BadClass.persist2() {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "}",
+                        
+                        "package r;\n" +
+                        "import p.BadClass;\n" +
+                        "import java.util.List;\n" + 
+                        "import java.util.ArrayList;\n" + 
+                        "public class Other {\n" +
+                        "    void g() { \n" + 
+                        "        List<Comparable<?>> v1 = new BadClass().persist(); \n" + 
+                        "        ArrayList<Comparable<?>> v2 = new BadClass().persist(); \n" + 
+                        "        String v3 = new BadClass().persist2(); \n" + 
+                        "        BadClass v4 = new BadClass().persist2(); \n" + 
+                        "        System.out.println(\"\"+v1+v2+v3+v4); \n" + 
+                        "    }\n" + 
+                        "}"});
+    }
+    
+    // test type parameters on method
+    // aspect and class in different file and references in different file
+    // now look at type parameters as arguments, not just return types
+    public void testBug343001d() throws Exception {
+        assertNoProblems(
+                new String[] { "p", "q", "r" }, 
+                new String[] { "BadClass.java", "Foo.aj", "Other.java" }, 
+                new String[] {
+                        "package p;\n" +
+                        "import java.util.List;\n" + 
+                        "import java.util.ArrayList;\n" + 
+                        "public class BadClass {\n" + 
+                        "    void g() { \n" + 
+                        "        List<Comparable<?>> v1 = new BadClass().persist(new ArrayList<Comparable<?>>()); \n" + 
+                        "        ArrayList<Comparable<?>> v2 = new BadClass().persist(new ArrayList<Comparable<?>>()); \n" + 
+                        "        String v3 = new BadClass().persist2(\"\"); \n" + 
+                        "        BadClass v4 = new BadClass().persist2(new BadClass()); \n" + 
+                        "        System.out.println(\"\"+v1+v2+v3+v4); \n" + 
+                        "    }\n" + 
+                        "}",
+                        
+                        "package q;\n" +
+                        "import java.util.List;\n" + 
+                        "import p.BadClass;\n" +
+                        "aspect Foo {\n" + 
+                        "    public <T extends List<Comparable<?>>> T BadClass.persist(T a) {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "    public <T> T BadClass.persist2(T a) {\n" + 
+                        "        return null;\n" + 
+                        "    }\n" + 
+                        "}",
+                        
+                        "package r;\n" +
+                        "import p.BadClass;\n" +
+                        "import java.util.List;\n" + 
+                        "import java.util.ArrayList;\n" + 
+                        "public class Other {\n" +
+                        "    void g() { \n" + 
+                        "        List<Comparable<?>> v1 = new BadClass().persist(new ArrayList<Comparable<?>>()); \n" + 
+                        "        ArrayList<Comparable<?>> v2 = new BadClass().persist(new ArrayList<Comparable<?>>()); \n" + 
+                        "        String v3 = new BadClass().persist2(\"\"); \n" + 
+                        "        BadClass v4 = new BadClass().persist2(new BadClass()); \n" + 
+                        "        System.out.println(\"\"+v1+v2+v3+v4); \n" + 
+                        "    }\n" + 
+                        "}"});
+    }
 }
