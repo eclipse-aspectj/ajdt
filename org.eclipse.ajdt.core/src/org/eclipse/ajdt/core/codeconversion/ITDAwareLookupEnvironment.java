cf70265a54cc2b67fe0e8dacbf1bb6d95eb87f1b
Andrew Eisenberg <andrew@eisenberg.as>
2013-01-14 16:09:28 -0800

    Initial work getting AJDT running on 4.3

--- org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/codeconversion/ITDAwareLookupEnvironment.java
+++ org.eclipse.ajdt.core/src/org/eclipse/ajdt/core/codeconversion/ITDAwareLookupEnvironment.java
@@ -13,6 +13,7 @@ import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.compiler.ast.CompilationUnitDeclaration;
 import org.eclipse.jdt.internal.compiler.env.INameEnvironment;
+import org.eclipse.jdt.internal.compiler.lookup.IQualifiedTypeResolutionListener;
 import org.eclipse.jdt.internal.compiler.lookup.LookupEnvironment;
 import org.eclipse.jdt.internal.core.search.matching.PossibleMatch;
 
@@ -24,6 +25,9 @@ public class ITDAwareLookupEnvironment extends LookupEnvironment {
     
     public ITDAwareLookupEnvironment(LookupEnvironment wrapper, INameEnvironment nameEnvironment) {
         super(wrapper.typeRequestor, wrapper.globalOptions, wrapper.problemReporter, nameEnvironment);
+        for (IQualifiedTypeResolutionListener l : wrapper.resolutionListeners) {
+            this.addResolutionListener(l);
+        }
     }
     
     
