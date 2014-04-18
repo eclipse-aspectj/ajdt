932786188df0cae155ee9ef91277e13d6de0601d
Andy Clement <aclement@gopivotal.com>
2014-04-17 17:35:04 -0700

    1.8.0.20140417150000

--- org.aspectj.weaver/source/org/aspectj/bridge/Version.java
+++ org.aspectj.weaver/source/org/aspectj/bridge/Version.java
@@ -30,14 +30,14 @@ public class Version {
     public static final long NOTIME = 0L;
     
     /** set by build script */
-    public static final String text = "1.8.0.RC3";
+    public static final String text = "1.8.0";
     // VersionUptodate.java scans for "static final String text = "
     
     /** 
       * Time text set by build script using SIMPLE_DATE_FORMAT.
       * (if DEVELOPMENT version, invalid)
       */
-    public static final String time_text = "Thursday Apr 10, 2014 at 00:02:53 GMT";
+    public static final String time_text = "Thursday Apr 17, 2014 at 20:04:16 GMT";
 
     /** 
       * time in seconds-since-... format, used by programmatic clients.
