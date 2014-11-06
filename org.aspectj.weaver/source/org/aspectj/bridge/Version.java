f4912ba0047df61fc0532697194187b37e0ca396
Andy Clement <aclement@gopivotal.com>
2014-11-06 13:32:33 -0800

    AspectJ 1.8.4

--- org.aspectj.weaver/source/org/aspectj/bridge/Version.java
+++ org.aspectj.weaver/source/org/aspectj/bridge/Version.java
@@ -37,7 +37,7 @@ public class Version {
       * Time text set by build script using SIMPLE_DATE_FORMAT.
       * (if DEVELOPMENT version, invalid)
       */
-    public static final String time_text = "Friday Oct 31, 2014 at 01:00:22 GMT";
+    public static final String time_text = "Thursday Nov 6, 2014 at 21:18:43 GMT";
 
     /** 
       * time in seconds-since-... format, used by programmatic clients.
