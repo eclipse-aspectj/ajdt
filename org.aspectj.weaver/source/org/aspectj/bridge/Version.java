eb2f34ff770a289ff85a056654f288953a009a05
Andy Clement <aclement@pivotal.io>
2015-09-03 14:30:07 -0700

    1.8.7.20150903142800

--- org.aspectj.weaver/source/org/aspectj/bridge/Version.java
+++ org.aspectj.weaver/source/org/aspectj/bridge/Version.java
@@ -37,7 +37,7 @@ public class Version {
       * Time text set by build script using SIMPLE_DATE_FORMAT.
       * (if DEVELOPMENT version, invalid)
       */
-    public static final String time_text = "Monday Jun 29, 2015 at 18:26:11 GMT";
+    public static final String time_text = "Thursday Sep 3, 2015 at 21:25:56 GMT";
 
     /** 
       * time in seconds-since-... format, used by programmatic clients.
