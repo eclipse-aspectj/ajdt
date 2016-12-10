7c4cf52e2194416ea7e7ffe1b5c9a47069308ca5
Andy Clement <aclement@pivotal.io>
2016-12-09 16:59:01 -0800

    final 1.8.10 tweaks

--- org.aspectj.weaver/source/org/aspectj/bridge/Version.java
+++ org.aspectj.weaver/source/org/aspectj/bridge/Version.java
@@ -37,7 +37,7 @@ public class Version {
       * Time text set by build script using SIMPLE_DATE_FORMAT.
       * (if DEVELOPMENT version, invalid)
       */
-    public static final String time_text = "Thursday Dec 8, 2016 at 20:40:27 GMT";
+    public static final String time_text = "Saturday Dec 10, 2016 at 00:50:44 GMT";
 
     /** 
       * time in seconds-since-... format, used by programmatic clients.
